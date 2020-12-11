a0f5f4a584891e986e2d6e76ccb5056b3f793dc3
someone0815 <hexx.heyy@web.de>
2020-12-11 21:44:51 +0100

    initial setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import fa from './components/globals/Fa.vue'
+import VueLodash from 'vue-lodash'
+import lodash from 'lodash'
+
+Vue.use(VueLodash, { name: 'custom', lodash: lodash });
 
 Vue.config.productionTip = false
 
+Vue.component('fa', fa);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
